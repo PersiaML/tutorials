65ae246b03b09ff53a950643b3d5bc677cf80a1f
Xiangru Lian <admin@mail.xrlian.com>
2021-09-07 03:47:12 -0700

    chore: update build script

--- ci/build.sh
+++ ci/build.sh
@@ -5,10 +5,9 @@ set -euo pipefail
 wget 'https://github.com/rust-lang/mdBook/releases/download/v0.4.12/mdbook-v0.4.12-x86_64-unknown-linux-gnu.tar.gz'
 tar xzf mdbook-v0.4.12-x86_64-unknown-linux-gnu.tar.gz
 
-wget 'https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/download/v0.7.4/mdbook-linkcheck.v0.7.4.x86_64-unknown-linux-gnu.zip'
-unzip -n mdbook-linkcheck.v0.7.4.x86_64-unknown-linux-gnu.zip
-
-chmod +x mdbook-linkcheck
+# wget 'https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/download/v0.7.4/mdbook-linkcheck.v0.7.4.x86_64-unknown-linux-gnu.zip'
+# unzip -n mdbook-linkcheck.v0.7.4.x86_64-unknown-linux-gnu.zip
+# chmod +x mdbook-linkcheck
 
 wget 'https://github.com/lzanini/mdbook-katex/releases/download/v0.2.10/mdbook-katex-v0.2.10-x86_64-unknown-linux-musl.tar.gz'
 tar xzf mdbook-katex-v0.2.10-x86_64-unknown-linux-musl.tar.gz
