22332f7dc58ff5045735768562603a921241bba9
Xiangru Lian <admin@mail.xrlian.com>
2021-09-07 03:41:36 -0700

    chore: update build script

--- ci/build.sh
+++ ci/build.sh
@@ -10,4 +10,10 @@ unzip -n mdbook-linkcheck.v0.7.4.x86_64-unknown-linux-gnu.zip
 
 chmod +x mdbook-linkcheck
 
+wget 'https://github.com/lzanini/mdbook-katex/releases/download/v0.2.10/mdbook-katex-v0.2.10-x86_64-unknown-linux-musl.tar.gz'
+tar xzf mdbook-katex-v0.2.10-x86_64-unknown-linux-musl.tar.gz
+
+wget 'https://github.com/badboy/mdbook-open-on-gh/releases/download/2.0.1/mdbook-open-on-gh-2.0.1-x86_64-unknown-linux-musl.tar.gz'
+tar xzf mdbook-open-on-gh-2.0.1-x86_64-unknown-linux-musl.tar.gz
+
 env PATH=.:$PATH mdbook build
